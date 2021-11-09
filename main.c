f3b0752a8775477d8a550f1f260dd898d9e4077b
hyna <alexisna@naver.com>
2021-11-09 18:08:52 +0900

    tested_memset

--- main.c
+++ main.c
@@ -9,27 +9,46 @@ int ft_isalnum(int c);
 int ft_isascii(int c);
 int ft_isprint(int c);
 size_t ft_strlen(char *s);
+void ft_memset(void *s, int c, size_t n);
+
 
 int main(void)
 {
-    char    arr[4] = "Aa0" ;
+	char	arr[4] = "Aa0" ;
+	char	testmemset[] = "testing";
+	char	ft_testmemset[] = "testing";
 
-    printf("isalpha    : A %d | a %d | 0 %d \n", isalpha(arr[0]), isalpha(arr[1]), isalpha(arr[2]));
-    printf("ft_isalpha : A %d | a %d | 0 %d \n", ft_isalpha(arr[0]), ft_isalpha(arr[1]), ft_isalpha(arr[2]));
+	printf("isalpha    : A %d | a %d | 0 %d \n", isalpha(arr[0]), isalpha(arr[1]), isalpha(arr[2]));
+	printf("ft_isalpha : A %d | a %d | 0 %d \n", ft_isalpha(arr[0]), ft_isalpha(arr[1]), ft_isalpha(arr[2]));
 
-    printf("isdigit    : A %d | a %d | 0 %d \n", isdigit(arr[0]), isdigit(arr[1]), isdigit(arr[2]));
-    printf("ft_isdigit : A %d | a %d | 0 %d \n", ft_isdigit(arr[0]), ft_isdigit(arr[1]), ft_isdigit(arr[2]));
-    
-    printf("isalnum    : A %d | a %d | 0 %d \n", isalnum(arr[0]), isalnum(arr[1]), isalnum(arr[2]));
-    printf("ft_isalnum : A %d | a %d | 0 %d \n", ft_isalnum(arr[0]), ft_isalnum(arr[1]), ft_isalnum(arr[2]));
-    
-    printf("isascii    : A %d | a %d | 0 %d \n", isascii(arr[0]), isascii(arr[1]), isascii(arr[2]));
-    printf("ft_isascii : A %d | a %d | 0 %d \n", ft_isascii(arr[0]), ft_isascii(arr[1]), ft_isascii(arr[2]));
-    
-    printf("isprint    : A %d | a %d | 0 %d \n", isprint(arr[0]), isprint(arr[1]), isprint(arr[2]));
-    printf("ft_isprint : A %d | a %d | 0 %d \n", ft_isprint(arr[0]), ft_isprint(arr[1]), ft_isprint(arr[2]));
-    
-    printf("strlen     : strlen(arr)    %ld \n", strlen(arr));
-    printf("ft_strlen  : ft_strlen(arr) %ld \n", ft_strlen(arr));
-    return (0);
+	printf("isdigit    : A %d | a %d | 0 %d \n", isdigit(arr[0]), isdigit(arr[1]), isdigit(arr[2]));
+	printf("ft_isdigit : A %d | a %d | 0 %d \n", ft_isdigit(arr[0]), ft_isdigit(arr[1]), ft_isdigit(arr[2]));
+
+	printf("isalnum    : A %d | a %d | 0 %d \n", isalnum(arr[0]), isalnum(arr[1]), isalnum(arr[2]));
+	printf("ft_isalnum : A %d | a %d | 0 %d \n", ft_isalnum(arr[0]), ft_isalnum(arr[1]), ft_isalnum(arr[2]));
+
+	printf("isascii    : A %d | a %d | 0 %d \n", isascii(arr[0]), isascii(arr[1]), isascii(arr[2]));
+	printf("ft_isascii : A %d | a %d | 0 %d \n", ft_isascii(arr[0]), ft_isascii(arr[1]), ft_isascii(arr[2]));
+
+	printf("isprint    : A %d | a %d | 0 %d \n", isprint(arr[0]), isprint(arr[1]), isprint(arr[2]));
+	printf("ft_isprint : A %d | a %d | 0 %d \n", ft_isprint(arr[0]), ft_isprint(arr[1]), ft_isprint(arr[2]));
+
+	printf("strlen     : strlen(arr)    %ld \n", strlen(arr));
+	printf("ft_strlen  : ft_strlen(arr) %ld \n", ft_strlen(arr));
+	
+	printf("memset     : %s \n", testmemset);
+	printf("ft_memset  : %s \n", ft_testmemset);
+
+	memset(testmemset, 'a', 7);
+	ft_memset(ft_testmemset, 'a', 7);
+
+	printf("memset     : %s \n", testmemset);
+	printf("ft_memset  : %s \n", ft_testmemset);
+	
+	memset(testmemset, '0', 4);
+	ft_memset(ft_testmemset, '0', 4);
+
+	printf("memset     : %s \n", testmemset);
+	printf("ft_memset  : %s \n", ft_testmemset);
+	return (0);
 }
