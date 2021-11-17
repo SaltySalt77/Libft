7cc1dd8e9fcae3eb58dbad8821efbfa94dc87d93
Hyeon Na <hyna@c7r1s9.42seoul.kr>
2021-11-17 18:20:44 +0900

    do ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hyna <hyna@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 15:36:33 by hyna              #+#    #+#             */
-/*   Updated: 2021/11/17 16:44:20 by hyna             ###   ########.fr       */
+/*   Updated: 2021/11/17 16:57:26 by hyna             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,4 @@
 
 char	*ft_strtrim(char const	*s1, char const	*set)
 {
-	char	*tmp;
-	size_t	i;
-	size_t	j;
-	size_t	len;
-
-	i = 0;
-	j = 0;
-	len = 0;
-	while (i < ft_strlen(s1))
-	{
-		if (!(ft_strchr(set, s1[i])))
-			len++;
-		i++;
-	}
-	tmp = (char *) ft_calloc(len + 1, len + 1);
-	if (tmp == NULL)
-		return (NULL);
-	i = 0;
-	while (j < len)
-	{
-		if (!(ft_strchr(set, s1[i])))
-			tmp[j++] = s1[i];
-		i++;
-	}
-	return (tmp);
 }
