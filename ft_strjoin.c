6f65b45986869991e1d970312e908a414b3c0fd3
Hyeon Na <hyna@c5r5s1.42seoul.kr>
2021-11-22 17:50:39 +0900

    do NULL

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: hyna <hyna@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 10:19:24 by hyna              #+#    #+#             */
-/*   Updated: 2021/11/17 15:29:56 by hyna             ###   ########.fr       */
+/*   Updated: 2021/11/22 17:50:08 by hyna             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strjoin(char const	*s1, char const *s2)
 	s1len = ft_strlen(s1);
 	s2len = ft_strlen(s2);
 	len = s1len + s2len;
-	result = (char *) malloc(len + 1);
+	result = (char *) ft_calloc(len + 1, 1);
 	if (result == NULL)
 		return (NULL);
 	pos = -1;
@@ -32,6 +32,5 @@ char	*ft_strjoin(char const	*s1, char const *s2)
 	pos = 0;
 	while (pos < s2len)
 		result[s1len++] = s2[pos++];
-	result[s1len] = 0;
 	return (result);
 }
