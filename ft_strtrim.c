0b6b6aa52162458e9ca17566e2210fac567578a8
Hyeon Na <hyna@c5r5s5.42seoul.kr>
2021-11-22 11:07:48 +0900

    tested ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hyna <hyna@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 15:36:33 by hyna              #+#    #+#             */
-/*   Updated: 2021/11/17 16:57:26 by hyna             ###   ########.fr       */
+/*   Updated: 2021/11/22 11:06:05 by hyna             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,16 @@
 
 char	*ft_strtrim(char const	*s1, char const	*set)
 {
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = ft_strlen(s1) - 1;
+	while (s1[i] && ft_strchr(set, (int) s1[i]))
+		i++;
+	if (i == ft_strlen(s1))
+		return ((char *) calloc(1, sizeof(char)));
+	while (s1[j] && ft_strrchr(set, (int) s1[j]))
+		j--;
+	return (ft_substr(s1, i, j - i + 1));
 }
