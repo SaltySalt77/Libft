e4d60de1ee5ad333a799805c36144a077fa70814
hyna <alexisna@naver.com>
2021-11-25 23:47:29 +0900

    mola

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: hyna <hyna@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 18:36:44 by hyna              #+#    #+#             */
-/*   Updated: 2021/11/22 11:21:27 by hyna             ###   ########.fr       */
+/*   Updated: 2021/11/25 18:44:45 by hyna             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 void	*ft_calloc(size_t	count, size_t	size)
 {
 	unsigned char	*result;
-	size_t			pos;
 
-	pos = 0;
 	result = (unsigned char *) malloc(count * size);
 	if (result == NULL)
 		return (NULL);
--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: hyna <hyna@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 16:17:31 by hyna              #+#    #+#             */
-/*   Updated: 2021/11/24 16:47:57 by hyna             ###   ########.fr       */
+/*   Updated: 2021/11/25 18:52:09 by hyna             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 void	ft_lstclear(t_list	**lst, void (*del)(void *))
 {
+	t_list	*head;
+
+	if (!lst || !*lst || !del)
+		return ;
+	while (*lst)
+	{
+		tmp = (*lst)->next;
+
 }
 
